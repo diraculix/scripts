e086d3093359cb1c7b393d6d514c413b19b99059
Lukas <lukaswolter98@gmail.com>
2022-05-28 15:03:47 +0200

    minor changes to machine_learning/autograd.py

--- machine_learning/autograd.py
+++ machine_learning/autograd.py
@@ -1,6 +1,16 @@
+from re import X
+from traceback import print_tb
 import torch
 
-if torch.cuda.is_available(): # check if CUDA GPU support is available (False)
+# check if CUDA GPU support is available (False for most notebooks)
+if torch.cuda.is_available(): 
 	print('CUDA support is available for current system')
 else:
 	print('No CUDA support available for current system')
+
+# use torch's autograd function
+
+x = torch.ones(3, 3, requires_grad=True)
+y = x + 2
+
+print(y)
