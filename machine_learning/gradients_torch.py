corrected wrong gradient definition in gradients_numpy.py | added and finished gradients_torch.py

machine_learning/gradients_torch.py
@@ -1,13 +1,13 @@
-import numpy as np
+import torch
 
  # f = w * x
  # f = 2 * x
 
 # sample training data (fixed)
-X = np.array([1, 2, 3, 4], dtype=np.float32)
-Y = np.array([2, 4, 6, 8], dtype=np.float32)
+X = torch.tensor([1, 2, 3, 4], dtype=torch.float32)
+Y = torch.tensor([2, 4, 6, 8], dtype=torch.float32)
 
-w = 0.
+w = torch.tensor(0., dtype=torch.float32, requires_grad=True)
 
 # model prediction
 def forward(x):
@@ -15,13 +15,7 @@ def forward(x):
 
 # loss ... MSE (linear regression)
 def loss(y, y_pred):
-    return ((y - y_pred)**2).mean()
-
-# gradient a.k.a. derivative of loss function
-# MSE = 1/N 2x (w*x - y)**2
-# dJ/dw = 1/N 2x (w*x - y)
-def gradient(x, y, y_pred):
-    return np.dot(2 * x, y_pred - y) / len(x)
+    return ((y - y_pred)**2).mean() # same syntax as in numpy
 
 print(f'Prediction before training: f(5) = {forward(5):.3f}')
 
@@ -36,15 +30,19 @@ for epoch in range(n_iters):
     # loss
     l = loss(Y, y_pred)
 
-    # gradients
-    dw = gradient(X, Y, y_pred)
+    # gradients ... now backward pass
+    l.backward() # gradient of loss dl/dw
 
-    # update weights
-    w -= learning_rate * dw
+    # update weights ... but do not include this in graph
+    with torch.no_grad():
+        w -= learning_rate * w.grad
+
+    # zero gradients
+    w.grad.zero_()
 
     if (epoch + 1) % 1 == 0:
         print(f'epoch {epoch + 1}: w = {w:.3f}, loss = {l:.8f}')
 
 print(f'Prediction after training: f(5) = {forward(5):.3f}')
 
-### now get rid of manual gradient definition
+### now replace manual weight updater with PyTorch optimizer