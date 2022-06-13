7fc3a3912838e20202196e38c878d225c40c9d3f
Lukas <lukaswolter98@gmail.com>
2022-06-13 09:16:10 +0200

    Finished training_pipeline.py | Completely replaced manual functions by model,loss,optimizer

--- machine_learning/training_pipeline.py
+++ machine_learning/training_pipeline.py
@@ -9,29 +9,29 @@ import torch
 import torch.nn as nn
 
 # sample training data (fixed)
-X = torch.tensor([1, 2, 3, 4], dtype=torch.float32)
-Y = torch.tensor([2, 4, 6, 8], dtype=torch.float32)
+X = torch.tensor([[1], [2], [3], [4]], dtype=torch.float32)
+Y = torch.tensor([[2], [4], [6], [8]], dtype=torch.float32)
 
-w = torch.tensor(0., dtype=torch.float32, requires_grad=True)
+n_samples, n_features = X.shape
 
+# model, automatically compute forward pass and weights
+# Linear requires 2D-array (numbers, features)
+in_size = n_features
+out_size = n_features
+model = nn.Linear(in_size, out_size)
 
-# model prediction
-def forward(x):
-    return w * x
-
-
-print(f'Prediction before training: f(5) = {forward(5):.3f}')
+print(f'Prediction before training: f(5) = {model(torch.tensor([5], dtype=torch.float32)).item():.3f}')
 
 # training
 learning_rate = 0.01
-n_iters = 10
+n_iters = 2000
 
 loss = nn.MSELoss()
-optimizer = torch.optim.SGD([w], lr=learning_rate)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 for epoch in range(n_iters):
     # prediction/forward pass
-    y_pred = forward(X)
+    y_pred = model(X)
 
     # loss (torch MSE)
     l = loss(y_pred, Y)
@@ -43,9 +43,10 @@ for epoch in range(n_iters):
     optimizer.step()
 
     # zero weights
-    w.grad.zero_()
+    model.zero_grad()
 
-    if (epoch + 1) % 1 == 0:
-        print(f'epoch {epoch + 1}: w = {w:.3f}, loss = {l:.8f}')
+    if (epoch + 1) % 200 == 0:
+        [w, b] = model.parameters()
+        print(f'epoch {epoch + 1}: weight = {w[0][0].item():.8f}, loss = {l:.8f}')
 
-print(f'Prediction after training: f(5) = {forward(5):.3f}')
+print(f'Prediction before training: f(5) = {model(torch.tensor([5], dtype=torch.float32)).item():.3f}')
