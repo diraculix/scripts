36bcf5939a65bfd96ff3f7165aad917bba28d390
diraculix <105131767+diraculix@users.noreply.github.com>
2022-07-26 16:17:19 +0200

    Added stdev( ) error bars to layer plots
    Observation: Huge deviations in some spot region's (x,y)-positions!

--- OART/logfile_extractor.py
+++ OART/logfile_extractor.py
@@ -1,7 +1,5 @@
 import os
 import sys
-import tkinter
-import numpy as np
 import pandas as pd
 from tkinter import Tk, filedialog
 from matplotlib import pyplot as plt
@@ -59,11 +57,11 @@ class MachineLog:
                 elif fname.__contains__(f'{self.patient_id}_tunings') and fname.endswith('.csv'):
                     self.tuning_df_name = fname
                     print(f'''Found patient tuning dataframe '{self.tuning_df_name}', reading in..''')
-                    self.patient_tuning_df = pd.read_csv(os.path.join(dirpath, fname), index_col='TIME')
+                    self.patient_tuning_df = pd.read_csv(os.path.join(dirpath, fname), index_col='TIME', dtype={'BEAM_ID': str})
                     tuning_df_exists = True
         
         if not record_df_exists or not tuning_df_exists:
-            print(f'''\nUnable to locate patient record/tuning dataframes.\nPlease run MachineLog.prepare_dataframe()''')                
+            print(f'''\nUnable to locate patient record/tuning dataframes for patient-ID {self.patient_id}.\nPlease run MachineLog.prepare_dataframe()''')                
         
         os.chdir(self.logfile_dir)
         
@@ -140,6 +138,7 @@ class MachineLog:
 
                     for tuning_file in tunings:
                         if int(tuning_file.split('_')[2].split('_')[0]) == layer_id:
+                            print('\t', tuning_file)
                             tuning_file_df = pd.read_csv(tuning_file, delimiter=',', skiprows=10, skipfooter=11, engine='python')
                             tuning_file_df['TIME'] = pd.to_datetime(tuning_file_df['TIME'])
                             tuning_file_df.index = tuning_file_df['TIME']
@@ -202,6 +201,10 @@ class MachineLog:
         print('Complete')
     
     def plot_beam_layers(self):
+        if self.patient_record_df.empty or self.patient_tuning_df.empty:
+            print(f'''\nUnable to locate patient record/tuning dataframes for patient-ID {self.patient_id}.\nPlease run MachineLog.prepare_dataframe()''')                
+            self.prepare_dataframe()
+
         beam_list = self.patient_record_df['BEAM_ID'].drop_duplicates()
         indices = beam_list.index.to_list()
 
@@ -223,216 +226,79 @@ class MachineLog:
                 print('Invalid input, try again..')
 
         beam_id = str(beam_list[key - 1])
-        del beam_list, indices
         scope_record_df = self.patient_record_df.loc[self.patient_record_df['BEAM_ID'] == beam_id]
         scope_tuning_df = self.patient_tuning_df.loc[self.patient_tuning_df['BEAM_ID'] == beam_id]
         print('Selected beam is:', beam_id)
 
         print('Generating plot..')
-        fig, axs = plt.subplots(7, 7, sharex=True, sharey=True, figsize=(24, 24), dpi=100)
+        fig, axs = plt.subplots(6, 8, sharex=True, sharey=True, figsize=(24, 24 * 6/8), dpi=150)
         ax0 = fig.add_subplot(111, frameon=False)
         fig.subplots_adjust(hspace=0.0, wspace=0.0)
         axs = axs.ravel()
         for layer_id in scope_record_df['LAYER_ID'].drop_duplicates():
-            x_positions, y_positions = [], []
-            x_tunings , y_tunings = [], []
+            x_positions, y_positions, x_std, y_std = [], [], [], []
+            x_tunings, y_tunings, tx_std, ty_std = [], [], [], []
             for spot_id in scope_record_df.loc[scope_record_df['LAYER_ID'] == layer_id]['SPOT_ID'].drop_duplicates():
                 x_positions.append(scope_record_df.loc[(scope_record_df['LAYER_ID'] == layer_id) & (scope_record_df['SPOT_ID'] == spot_id)]['X_POSITION(mm)'].mean())
+                x_std.append(scope_record_df.loc[(scope_record_df['LAYER_ID'] == layer_id) & (scope_record_df['SPOT_ID'] == spot_id)]['X_POSITION(mm)'].std())
                 y_positions.append(scope_record_df.loc[(scope_record_df['LAYER_ID'] == layer_id) & (scope_record_df['SPOT_ID'] == spot_id)]['Y_POSITION(mm)'].mean())
+                y_std.append(scope_record_df.loc[(scope_record_df['LAYER_ID'] == layer_id) & (scope_record_df['SPOT_ID'] == spot_id)]['Y_POSITION(mm)'].std())
             for tuning_id in scope_tuning_df.loc[scope_tuning_df['LAYER_ID'] == layer_id]['SPOT_ID'].drop_duplicates():   
                 x_tunings.append(scope_tuning_df.loc[(scope_tuning_df['LAYER_ID'] == layer_id) & (scope_tuning_df['SPOT_ID'] == tuning_id)]['X_POSITION(mm)'].mean())
+                tx_std.append(scope_tuning_df.loc[(scope_tuning_df['LAYER_ID'] == layer_id) & (scope_tuning_df['SPOT_ID'] == tuning_id)]['X_POSITION(mm)'].std())
                 y_tunings.append(scope_tuning_df.loc[(scope_tuning_df['LAYER_ID'] == layer_id) & (scope_tuning_df['SPOT_ID'] == tuning_id)]['Y_POSITION(mm)'].mean())
+                ty_std.append(scope_tuning_df.loc[(scope_tuning_df['LAYER_ID'] == layer_id) & (scope_tuning_df['SPOT_ID'] == tuning_id)]['Y_POSITION(mm)'].std())
             
-            axs[layer_id].plot(
-                x_positions,
-                y_positions,
+            axs[layer_id].errorbar(
+                x=x_positions, 
+                y=y_positions, 
+                xerr=x_std, 
+                yerr=y_std,
                 label=f'Spot positions',
                 marker='o',
                 markeredgecolor='black',
-                markerfacecolor='None',
+                markerfacecolor='none',
                 markersize=1.5,
                 markeredgewidth=0.2,
-                color='tab:blue',
-                linewidth=0.2
+                linewidth=0.2,
+                color='tab:blue', 
+                ecolor='tab:red', 
+                elinewidth=0.2, 
+                capsize=1.0, 
+                capthick=0.2
             )
-            axs[layer_id].scatter(x_tunings, y_tunings, s=1.5, color='limegreen', label='Tuning positions')
-            axs[layer_id].annotate(f'Layer #{str(layer_id + 1).zfill(2)}', xy=(1.0, 1.0), xycoords='axes points', fontsize=8)
+            axs[layer_id].errorbar(
+                x=x_tunings, 
+                y=y_tunings, 
+                xerr=tx_std, 
+                yerr=ty_std,
+                label=f'Tuning positions',
+                marker='o',
+                markeredgecolor='limegreen',
+                markerfacecolor='none',
+                markersize=1.5,
+                markeredgewidth=0.2,
+                linestyle='None',
+                ecolor='tab:red', 
+                elinewidth=0.2, 
+                capsize=1.0, 
+                capthick=0.2
+            )
+            axs[layer_id].annotate(f'Layer #{str(layer_id + 1).zfill(2)}', xy=(1.0, 1.0), xycoords='axes points', fontsize=10)
             axs[layer_id].legend(loc='upper right')
-        plt.suptitle(f'Spot Positions for Beam-ID: {beam_id}', fontweight='bold', y=0.9)
-        ax0.set_xlabel('X', fontweight='bold', labelpad=10)
-        ax0.set_ylabel('Y', fontweight='bold', labelpad=10)
+        plt.suptitle(f'Spot Positions for Patient-ID {self.patient_id}, Beam-ID: {beam_id}', fontweight='bold', y=0.9)
+        ax0.set_xlabel('X [mm]', fontweight='bold', labelpad=10)
+        ax0.set_ylabel('Y [mm]', fontweight='bold', labelpad=10)
         plt.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
         output_dir = r'N:\fs4-HPRT\HPRT-Docs\Lukas\Logfile_Extraction\output'
         if not os.path.isdir(output_dir):
             os.mkdir(output_dir)
-        try:
-            plt.savefig(f'{output_dir}/{self.patient_id}_{beam_id}_spots.pdf')
-        except PermissionError:
-            print(f'  Permission denied, no plot generated')
-
-        
-class Beam(MachineLog):
-    def __init__(self, log, fraction_id, beam_id) -> None:
-        current_beam_path = os.path.join(log.logfile_dir, fraction_id, beam_id)
-        os.chdir(current_beam_path)
-        while len(os.listdir('.')) <= 3:
-            os.chdir(os.listdir('.')[0])
-        
-        self.beam_id = beam_id
-        self.fraction_id = fraction_id
-        self.map_records, self.tunings, unused = [], [], []
-        for file in os.listdir('.'):
-            if file.__contains__('beam.'):
-                self.beam_file = file
-            elif file.__contains__('beam_config.'):
-                self.beam_config = file
-            elif file.__contains__('map_record') and file.__contains__('part'):
-                self.map_records.append(file)
-            elif file.__contains__('map_record') and file.__contains__('tuning'):
-                self.tunings.append(file)
-            else:
-                unused.append(file)
-
-        if len(self.map_records) == 0 or len(self.tunings) == 0:
-            raise LookupError('No logfiles found!')          
-
-        self.num_layers = max([int(fname.split('_')[2].split('_')[0]) for fname in self.map_records])
-        
-        with open(self.beam_file, 'r') as beam_file:
-            for line in beam_file.readlines():
-                if line.__contains__('GantryAngle'):
-                    self.gantry_angle = float(line.split('>')[1].split('<')[0])
-                elif line.__contains__('pressure'):
-                    pressure = float(line.split(',')[-1])
-                elif line.__contains__('temperature'):
-                    temperature = float(line.split(',')[-1])
-                
-            ref_pressure, ref_temperature = 1013., 293.
-            self.correction_factor = (ref_pressure * temperature) / (ref_temperature * pressure)
-            beam_file.close()
-        
-
-        
-        # print(f'\nBeam file: \n {self.beam_file}\nConfig file: \n {self.beam_config}\nMap records: \n {np.array(self.map_records)}\nTunings: \n {np.array(self.tunings)}\nUnused: \n {np.array(unused)}')
-
-    def collect_data(self, layers='all', plot=False, drop=True):
-        self.layer_spot_data, self.layer_tuning_data = [], []
-        if str(layers) == 'all':
-            layers = range(self.num_layers)
-        elif int(layers) > self.num_layers - 1:
-            print(f'Selected layer-ID [{layers}] exceeds highest layer-ID [{self.num_layers - 1}], using max. value instead..')
-            layers = [self.num_layers - 1]
-        else:
-            layers = [int(layers)]
-            
-        for layer_id in layers:
-            to_do_layers, to_do_tunings = [], []
-            for file in self.map_records:
-                if int(file.split('_')[2].split('_')[0]) == layer_id:
-                    to_do_layers.append(pd.read_csv(file, delimiter=',', skiprows=10, skipfooter=11, engine='python'))
-
-            for file in self.tunings:
-                if int(file.split('_')[2].split('_')[0]) == layer_id:
-                    to_do_tunings.append(pd.read_csv(file, delimiter=',', skiprows=10, skipfooter=11, engine='python'))
-            
-            for i in range(len(to_do_tunings)):
-                if i > 0:
-                    to_do_tunings[i]['SUBMAP_NUMBER'] += to_do_tunings[i - 1]['SUBMAP_NUMBER'].max()
-            for j in range(len(to_do_layers)):
-                if j > 0:
-                    to_do_layers[j]['SUBMAP_NUMBER'] += to_do_layers[j - 1]['SUBMAP_NUMBER'].max()
-            
-            layer_df = pd.concat(to_do_layers)
-            tuning_df = pd.concat(to_do_tunings)
-            del to_do_layers, to_do_tunings
-
-            layer_df['TIME'] = pd.to_datetime(layer_df['TIME'])
-            layer_df.index = layer_df['TIME']
-            layer_beamtime = layer_df['TIME'].iloc[-1] - layer_df['TIME'].iloc[0]
-            layer_df = layer_df.loc[:, :'Y_DOSE_RATE(A)']
-            layer_df = layer_df.loc[(layer_df['X_WIDTH(mm)'] != -10000.0) & (layer_df['Y_WIDTH(mm)'] != -10000.0) & (layer_df['X_POSITION(mm)'] != -10000.0) & (layer_df['Y_POSITION(mm)'] != -10000.0)]
-            layer_df.drop(columns=['TIME'], inplace=True)
-            tuning_df['TIME'] = pd.to_datetime(tuning_df['TIME'])
-            tuning_df.index = tuning_df['TIME']
-            tuning_beamtime = tuning_df['TIME'].iloc[-1] - tuning_df['TIME'].iloc[0]
-            tuning_df = tuning_df.loc[:, :'Y_DOSE_RATE(A)']
-            tuning_df = tuning_df.loc[(tuning_df['X_WIDTH(mm)'] != -10000.0) & (tuning_df['Y_WIDTH(mm)'] != -10000.0) & (tuning_df['X_POSITION(mm)'] != -10000.0) & (tuning_df['Y_POSITION(mm)'] != -10000.0)]
-            tuning_df.drop(columns=['TIME'], inplace=True)
-            
-            spot_dfs, tuning_dfs = [], []
-            while not layer_df.empty:  # mine dataframe until empty
-                current_spot = layer_df.loc[layer_df['SUBMAP_NUMBER'] == min(layer_df['SUBMAP_NUMBER'])]
-                layer_df.drop(current_spot.index, inplace=True)
-                spot_dfs.append(current_spot)
-            
-            while not tuning_df.empty:  # mine dataframe until empty
-                current_tune = tuning_df.loc[tuning_df['SUBMAP_NUMBER'] == min(tuning_df['SUBMAP_NUMBER'])]
-                tuning_df.drop(current_tune.index, inplace=True)
-                tuning_dfs.append(current_tune)
-
-            self.layer_spot_data.append(spot_dfs)
-            self.layer_tuning_data.append(tuning_dfs)
-            del spot_dfs, tuning_dfs
-        
-        if plot:
-            print('  ..Drawing..')
-            fig, axs = plt.subplots(7, 7, sharex=True, sharey=True, figsize=(24, 24), dpi=150)
-            ax0 = fig.add_subplot(111, frameon=False)
-            fig.subplots_adjust(hspace=0.0, wspace=0.0)
-            axs = axs.ravel()
-            for index, layer in enumerate(self.layer_spot_data):
-                layer_x_means, layer_y_means = [], []
-                layer_x_stds, layer_y_stds = [], []
-                tuning_x_means, tuning_y_means= [], []
-                tuning_x_stds, tuning_y_stds = [] ,[]
-                for spot in layer:
-                    layer_x_means.append(spot['X_POSITION(mm)'].mean())
-                    layer_x_stds.append(spot['X_POSITION(mm)'].std())
-                    layer_y_means.append(spot['Y_POSITION(mm)'].mean())
-                    layer_y_stds.append(spot['Y_POSITION(mm)'].std())
-                for spot in self.layer_tuning_data[index]:
-                    tuning_x_means.append(spot['X_POSITION(mm)'].mean())
-                    tuning_x_stds.append(spot['X_POSITION(mm)'].std())
-                    tuning_y_means.append(spot['Y_POSITION(mm)'].mean())
-                    tuning_y_stds.append(spot['Y_POSITION(mm)'].std())
-
-                axs[index].plot(layer_x_means, layer_y_means)
-                axs[index].plot(layer_x_means[0], layer_y_means[0], 'o', markeredgewidth=0.1, color='black', markerfacecolor='none')
-                # axs[index].quiver(layer_x_means[:1], layer_y_means[:1], layer_x_means[1]-layer_x_means[0], layer_y_means[1]-layer_y_means[0])#, scale_units='xy', angles='xy', scale=1)
-                axs[index].plot(layer_x_means, layer_y_means, '.', markersize=2.0, color='black', label=f'spots')
-                axs[index].plot(tuning_x_means, tuning_y_means, '.', markersize=2.0, color='limegreen', label=f'tunings')
-                if index == int(len(self.layer_spot_data) / 2):
-                    axs[index].set_xlim(np.median(layer_x_means) - 50, np.median(layer_x_means) + 50)
-                    axs[index].set_ylim(np.median(layer_y_means) - 50, np.median(layer_y_means) + 50)
-                # axs[index].errorbar(layer_x_means, layer_y_means, xerr=layer_x_stds, yerr=layer_y_stds)  #, xerr=layer_x_stds, yerr=layer_y_stds)
-                axs[index].legend(loc='upper right')
-                axs[index].annotate(f'Layer #{str(index + 1).zfill(2)}', xy=(1.0, 1.0), xycoords='axes points', fontsize=10)
-            plt.suptitle(f'Spot Positions for Beam-ID: {self.beam_id} in Fraction: {self.fraction_id}', fontweight='bold', y=0.9)
-            ax0.set_xlabel('X', fontweight='bold', labelpad=10)
-            ax0.set_ylabel('Y', fontweight='bold', labelpad=10)
-            plt.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
-            output_dir = r'N:\fs4-HPRT\HPRT-Docs\Lukas\Logfile_Extraction\output'
-            if not os.path.isdir(output_dir):
-                os.mkdir(output_dir)
+        while True:
             try:
-                plt.savefig(f'{output_dir}/{self.fraction_id}_{self.beam_id}_spots.pdf')
+                plt.savefig(f'{output_dir}/{self.patient_id}_{beam_id}_spots.pdf')
+                break
             except PermissionError:
-                print(f'  Permission denied, no plot generated')
-            
-            plt.cla()
-            plt.close(fig)
-        
-        if drop:
-            dfs_to_join = []
-            for layer_id in range(self.num_layers):
-                for spot_id, spot in enumerate(self.layer_spot_data[layer_id]):
-                    self.layer_spot_data[layer_id][spot_id]['FRACTION_ID'] = self.fraction_id
-                    self.layer_spot_data[layer_id][spot_id]['BEAM_ID'] = self.beam_id
-                    self.layer_spot_data[layer_id][spot_id]['LAYER_ID'] = layer_id
-                    self.layer_spot_data[layer_id][spot_id]['SPOT_ID'] = spot_id
-                
-                dfs_to_join.append(pd.concat(self.layer_spot_data[layer_id]))
-            log.fraction_beam_dfs.append(pd.concat(dfs_to_join))
+                input('  Permission denied, close target file and press ENTER.. ')
 
 
 if __name__ == '__main__':
